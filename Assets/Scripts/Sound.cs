cc0c88b87025e6d95c80e4b5b5a848f5ae911e7b
Yigit Yeg <yyeg214@gmail.com>
2023-05-23 16:54:00 +0300

    some bugs are fixed

--- Assets/Scripts/Sound.cs
+++ Assets/Scripts/Sound.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class Sound : MonoBehaviour
+public class Sound 
 {
    public string name;
    public AudioClip clip;
